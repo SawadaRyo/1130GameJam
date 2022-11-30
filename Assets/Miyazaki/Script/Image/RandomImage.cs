9b2bbbd5efa3fa5e875e46c94cdd6d917c10c3bf
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2022-11-30 16:20:19 +0900

    【Add】Image

---
+++ Assets/Miyazaki/Script/Image/RandomImage.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RandomImage : MonoBehaviour
+{
+    [SerializeField] Image _image;
+    [SerializeField] Sprite _sprite1;
+    [SerializeField] Sprite _sprite2;
+    [SerializeField] Sprite _sprite3;
+
+    private void Start()
+    {
+        RandomSprite();
+    }
+
+    void RandomSprite()
+    {
+        int rnd = UnityEngine.Random.Range(1, 3);
+        switch (rnd)
+        {
+            case 1:
+                _image.sprite = _sprite1;
+                break;
+            case 2:
+                _image.sprite = _sprite2;
+                break;
+            case 3:
+                _image.sprite = _sprite3;
+                break;
+        }
+    }
+}

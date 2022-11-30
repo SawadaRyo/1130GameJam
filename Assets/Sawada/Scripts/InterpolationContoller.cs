3eae0268a2328e797a779fa60cc42bf633443a80
SR21C <rysawad@gmail.com>
2022-11-30 13:53:37 +0900

    [add]ゲームマネージャー追加、プレイヤー追加

---
+++ Assets/Sawada/Scripts/InterpolationContoller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+
+//テキストで表示する値を補間し滑らかに表示する為のクラス
+public class InterpolationContoller
+{
+    [Tooltip("補間仕切るまでの時間")]
+    float _interpolationRaito = 0.1f;
+    [Tooltip("現在の値")]
+    float _currentValue = 0f;
+    [Tooltip("対象のText")]
+    Text _targetText = null;
+
+    public InterpolationContoller(float interpolationRaito, float currentValue, Text targetText)
+    {
+        _interpolationRaito = interpolationRaito;
+        _currentValue = currentValue;
+        _targetText = targetText;
+    }
+
+    //値の補間
+    public void ChangeValue(float endValue)
+    {
+        DOTween.To(() => _currentValue,
+            x => _currentValue = x
+            , endValue
+            , _interpolationRaito)
+            .OnUpdate(() => _targetText.text = "Score:" + _currentValue.ToString("D10"));
+    }
+}
